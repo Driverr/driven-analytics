e18037f655d1bbdbbb8b910dec71378754ccd516
Shivalik <shvlksen@gmail.com>
2016-08-01 19:12:36 +0530

    added CORS handling on server

--- server.js
+++ server.js
@@ -28,6 +28,28 @@ app.use(
 
 app.use(bodyParser.json({}));
 
+var enableCORS = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        //console.log("enablecors");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        //console.log("enablingcors");
+ // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    //res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        //console.log(JSON.stringify(req),null,4);
+    // intercept OPTIONS method
+    //if ('OPTIONS' == req.method) {
+     // res.send(200);
+    //}
+   // else {
+      next();
+//    }
+};
+
+app.use(enableCORS);
+
+
+
 var router = express.Router();
 
 
